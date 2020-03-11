e655bc7fd5c663efc056f9732b8a8789d9b5c0d4
Danika <danikachrunik@gmail.com>
2020-03-10 22:55:40 -0700

    Responsive footer and menu
    - changed css for footer to make it more responsive
    - smaller files for social media icons
    - removed some info from the footer
    - worked on drop down menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -8,27 +8,30 @@ menu.classList.add('hidden');
 menu.setAttribute('aria-hidden', 'true');
 menu.setAttribute('aria-labelledby', 'menu-toggle');
 
-var menuToggle = document.querySelector('menu-toggle');
+var menuToggle = document.querySelector('#menu-toggle');
 menuToggle.setAttribute('aria-label', 'Menu Toggle');
 menuToggle.setAttribute('aria-controls', 'menu');
 menuToggle.setAttribute('aria-expanded', 'false');
 
 var menuFirstItem = document.querySelector('#menu a:first-of-type');
-var header = document.querySelector('header');
 
 menuToggle.addEventListener('click',
 	function() {
 		if ( menu.classList.contains('hidden') ) {
 
 			menu.classList.remove('hidden');
+			//menu.classList.add('header-right');
 			menu.setAttribute('aria-hidden', 'false');
 			menu.setAttribute('aria-expanded', 'true');
 			menuFirstItem.focus();
+
 		} else {
 
 			menu.classList.add('hidden');
+			//menu.classList.remove('header-right');
 			menu.setAttribute('aria-hidden', 'true');
 			menu.setAttribute('aria-expanded', 'false');
+
 		}
 
 	}
